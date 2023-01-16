fde58fcb525bb8ecc13da4306f92c3edbfe399a6
mecarm <melecarmine99@gmail.com>
2023-01-16 18:15:13 +0100

    Style card

--- movie-laravel/resources/views/welcome.blade.php
+++ movie-laravel/resources/views/welcome.blade.php
@@ -4,7 +4,13 @@
 
 @section('main-content')
     <h1>MovieFix</h1>
+    <div style="display: flex; flex-wrap: wrap; justify-content:center;">
     @foreach ($all_movie as $elem)
-        <div>{{$elem['title']}}</div>
+            <div style="background-color: antiquewhite; width: calc((100% / 5) - 20px); margin: 10px; padding: 20px; border-radius: 20px;">
+                <h2>{{$elem['title']}}</h2>
+                <h5>Titolo originale: {{$elem['original_title']}}</h5>
+                <span>Voto: {{$elem['vote']}}</span>
+            </div>
     @endforeach
+    </div>
 @endsection
